6bc433879828aa516f8da706a96957fc5b2ecba1
William Estoque <Billy@Stokee.local>
2009-10-30 17:11:05 -0700

    Unpacked gems

--- config/environment.rb
+++ config/environment.rb
@@ -34,6 +34,15 @@ Rails::Initializer.run do |config|
   # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
   # Run "rake -D time" for a list of tasks for finding time zone names.
   config.time_zone = 'UTC'
+  config.gem 'authlogic'
+  config.gem "thoughtbot-factory_girl",
+               :lib    => "factory_girl",
+               :source => "http://gems.github.com"
+  config.gem 'rspec', :lib => false
+  config.gem 'rspec-rails', :lib => false
+  config.gem 'haml'
+  config.gem 'grit'
+  config.gem 'coderay'
 
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
