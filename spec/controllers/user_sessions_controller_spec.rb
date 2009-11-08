54efa25ec849321babadf31eb303011b2ac93505
William Estoque <Billy@Stokee.local>
2009-11-08 12:31:24 -0800

    Commit with tests working

--- spec/controllers/user_sessions_controller_spec.rb
+++ spec/controllers/user_sessions_controller_spec.rb
@@ -2,18 +2,13 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe UserSessionsController do
   
-  def populate_user_table
-    Factory.create(:valid_user)
-  end
-
   it "should get new user session" do
     get :new
-    assigns(:user_session).should_not be_nil
     response.should be_success
   end
 
   it "should create a new session if valid" do
-    populate_user_table
+    Factory.create(:valid_user)
     post :create, :user_session => Factory.attributes_for(:valid_user)
     response.should redirect_to(repositories_url)
   end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,29 +2,31 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe UsersController do
   
-  def populate_users_table
-    Factory.create(:valid_user)
+  describe "new action" do
+    it "should render new" do
+      get :new
+      assigns(:user).should_not be_nil
+      response.should be_success
+    end
   end
   
-  it "should GET/new view" do
-    get :new
-    assigns(:user).should_not be_nil
-    response.should be_success
+  describe "create action" do
+    it "should redirect after creating a valid user" do
+      post :create, :user => Factory.attributes_for(:valid_user)
+      response.should redirect_to(account_url)
+    end
   end
   
-  it "should POST/create when given valid data" do
-    post :create, :user => Factory.attributes_for(:valid_user)
-    response.should redirect_to(account_url)
-  end
-  
-  it "should not GET/show view if logged in" do
-    authenticate
-    get :show, :id => 1
-    response.should be_success
-  end
-  
-  it "should not GET/show view if not logged in" do
-    get :show, :id => 1
-    response.should_not be_success
+  describe "show action" do
+    it "should render show" do
+      authenticate
+      get :show, :id => 1
+      response.should be_success
+    end
+    
+    it "should redirect when not logged in" do
+      get :show, :id => 1
+      response.should be_redirect
+    end
   end
 end
